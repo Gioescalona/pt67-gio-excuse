50437420a0c834fa9c8cdd8a60b2afab2d20cf4e
Eligio Escalona <158042766+Gioescalona@users.noreply.github.com>
2024-04-06 10:57:34 +0000

    proyecto final

--- src/app.js
+++ src/app.js
@@ -5,7 +5,42 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = () => {
+  document.querySelector("#key").addEventListener("click", () => {
+    document.querySelector("#excuse").innerHTML = excuseGenerator();
+  });
   //write your code here
   console.log("Hello Rigo from the console!");
+};
+
+let excuseGenerator = () => {
+  let article = ["¡La", "¡Una"];
+  let substantive = ["niña", "yegua", "profesora", "compañera"];
+  let action = ["se comió", "parió", "no explicó", "suspendió"];
+  let thing = ["mi merienda", "un potrillo", "los deberes", "un moco"];
+  let where = [
+    "en el colegio!",
+    "en clase!",
+    "en el examen!",
+    "en la hípica!",
+    "en el establo!"
+  ];
+
+  let artHtml = Math.floor(Math.random() * article.length);
+  let subsHtml = Math.floor(Math.random() * substantive.length);
+  let actionHtml = Math.floor(Math.random() * action.length);
+  let thingHtml = Math.floor(Math.random() * thing.length);
+  let whereHtml = Math.floor(Math.random() * where.length);
+
+  return (
+    article[artHtml] +
+    " " +
+    substantive[subsHtml] +
+    " " +
+    action[actionHtml] +
+    " " +
+    thing[thingHtml] +
+    " " +
+    where[whereHtml]
+  );
 };
